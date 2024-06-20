9b9bfea07d3ed4efad915ca3bd89176727180b6c
samualhalder <samual.halder2020@gmail.com>
2024-06-20 23:53:45 +0530

    theme changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Skills from "./pages/Skills";
 
 function App() {
   return (
-    <div className="bg-gray-400 p-2">
+    <div className="bg-[#000] p-2">
       <BrowserRouter>
         <Header />
         <Home />
