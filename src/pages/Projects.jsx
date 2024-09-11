363de8aa5c03a280fc85d3ccf4fd8d75aa7ca8e6
samualhalder <samual.halder2020@gmail.com>
2024-09-12 00:40:47 +0530

    added project 3 & 4

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,14 +5,28 @@ function Projects() {
   const bgColor = import.meta.env.VITE_BGCOLOR;
   const projects = [
     {
-      id: 1,
+      id: 4,
+      title: "Mail chat",
+      photoURL: "mail-chat.png",
+      about: "Take feedback from your user and share them as a testimoianl.",
+      link: "https://mail-chat-rose.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Feedback Hub",
+      photoURL: "feedback-hub.png",
+      about: "Take feedback from your user and share them as a testimoianl.",
+      link: "https://feedback-hub-kung.onrender.com/",
+    },
+    {
+      id: 2,
       title: "TechNuze",
       photoURL: "techNuze-project.png",
       about: "its a fully functional news site build witd MERN.",
       link: "https://technuze.onrender.com/",
     },
     {
-      id: 2,
+      id: 1,
       title: "Hello mall",
       photoURL: "hellomall-project.png",
       about: "its a fully functional e-commerce site build witd MERN.",
