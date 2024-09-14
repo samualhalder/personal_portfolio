48757e34eb07b4dd88a7f90107a601fbfeb89d1c
samualhalder <samual.halder2020@gmail.com>
2024-09-15 00:06:17 +0530

    change in project card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,53 @@
-import React from "react";
+import { Button } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
+import { MdLiveTv } from "react-icons/md";
+import { FaReact } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+import { FaJs } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { SiRedis } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
 
 function Card({ project }) {
   return (
-    <Link to={project.link} target="_blank">
-      <div className=" font-sans m-10 max-w-[400px] mx-auto border-2 border-cyan-600 p-5 rounded-lg flex  flex-wrap items-center justify-center transform transition duration-1000 hover:scale-110 ">
-        <img className="m-5" src={project.photoURL} alt="image" />
-        <h1 className=" font-lobster text-3xl">{project.title}</h1>
-        <p className="text-xl text-gray-500 m-5">{project.about}</p>
+    <div className="font-sans m-10 max-w-[400px] mx-auto border-2 border-cyan-600 p-5 rounded-lg flex  flex-wrap  justify-center transform transition duration-1000 hover:scale-110 ">
+      <img className="m-5" src={project?.photoURL} alt="image" />
+      <div className="flex  justify-between w-full">
+        <h1 className=" font-lobster text-3xl">{project?.title}</h1>
+        <div className=" flex gap-1">
+          <Link to={project?.link}>
+            <Button className=" rounded-2xl">
+              <MdLiveTv size={20} />
+            </Button>
+          </Link>
+          <Link to={project?.github}>
+            <Button className="rounded-2xl">
+              <FaGithub size={20} />{" "}
+            </Button>
+          </Link>
+        </div>
       </div>
-    </Link>
+      <div className="flex gap-4 w-full my-2">
+        {project.tech.includes("react") && <FaReact size={25} />}
+        {project.tech.includes("next") && <RiNextjsFill size={25} />}
+        {project.tech.includes("js") && <FaJs size={25} />}
+        {project.tech.includes("ts") && <SiTypescript size={25} />}
+        {project.tech.includes("node") && <FaNodeJs size={25} />}
+        {project.tech.includes("express") && <SiExpress size={25} />}
+        {project.tech.includes("mongo") && <SiMongodb size={25} />}
+        {project.tech.includes("redis") && <SiRedis size={25} />}
+        {project.tech.includes("docker") && <FaDocker size={25} />}
+        {project.tech.includes("tailwind") && <RiTailwindCssFill size={25} />}
+      </div>
+      <p className="text-lg text-gray-500 my-5">{project?.about}</p>
+    </div>
   );
 }
 
 export default Card;
-90;
+6;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,38 +1,9 @@
 import React from "react";
 import Card from "../components/Card";
+import { projects } from "../../lib/projectsDb.js";
 
 function Projects() {
   const bgColor = import.meta.env.VITE_BGCOLOR;
-  const projects = [
-    {
-      id: 4,
-      title: "Mail chat",
-      photoURL: "mail-chat.png",
-      about: "A fast chat app using redis server.",
-      link: "https://mail-chat-rose.vercel.app/",
-    },
-    {
-      id: 3,
-      title: "Feedback Hub",
-      photoURL: "feedback-hub.png",
-      about: "Take feedback from your user and share them as a testimoianl.",
-      link: "https://feedback-hub-kung.onrender.com/",
-    },
-    {
-      id: 2,
-      title: "TechNuze",
-      photoURL: "techNuze-project.png",
-      about: "its a fully functional news site build witd MERN.",
-      link: "https://technuze.onrender.com/",
-    },
-    {
-      id: 1,
-      title: "Hello mall",
-      photoURL: "hello-mall.png",
-      about: "its a fully functional e-commerce site build witd MERN.",
-      link: "https://mern-ecommerce-delta-one.vercel.app/",
-    },
-  ];
 
   return (
     <div
