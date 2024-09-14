8b126a4d1e8bc06e77af6f63506e99d2e955deb6
samualhalder <samual.halder2020@gmail.com>
2024-09-15 00:15:14 +0530

    ui changes in card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,7 +18,7 @@ function Card({ project }) {
     <div className="font-sans m-10 max-w-[400px] mx-auto border-2 border-cyan-600 p-5 rounded-lg flex  flex-wrap  justify-center transform transition duration-1000 hover:scale-110 ">
       <img className="m-5" src={project?.photoURL} alt="image" />
       <div className="flex  justify-between w-full">
-        <h1 className=" font-lobster text-3xl">{project?.title}</h1>
+        <h1 className=" font-font-Arial text-3xl">{project?.title}</h1>
         <div className=" flex gap-1">
           <Link to={project?.link}>
             <Button className=" rounded-2xl">
@@ -32,17 +32,17 @@ function Card({ project }) {
           </Link>
         </div>
       </div>
-      <div className="flex gap-4 w-full my-2">
-        {project.tech.includes("react") && <FaReact size={25} />}
-        {project.tech.includes("next") && <RiNextjsFill size={25} />}
-        {project.tech.includes("js") && <FaJs size={25} />}
-        {project.tech.includes("ts") && <SiTypescript size={25} />}
-        {project.tech.includes("node") && <FaNodeJs size={25} />}
-        {project.tech.includes("express") && <SiExpress size={25} />}
-        {project.tech.includes("mongo") && <SiMongodb size={25} />}
-        {project.tech.includes("redis") && <SiRedis size={25} />}
-        {project.tech.includes("docker") && <FaDocker size={25} />}
-        {project.tech.includes("tailwind") && <RiTailwindCssFill size={25} />}
+      <div className="flex gap-3 w-full my-2">
+        {project.tech.includes("react") && <FaReact size={20} />}
+        {project.tech.includes("next") && <RiNextjsFill size={20} />}
+        {project.tech.includes("js") && <FaJs size={20} />}
+        {project.tech.includes("ts") && <SiTypescript size={20} />}
+        {project.tech.includes("node") && <FaNodeJs size={20} />}
+        {project.tech.includes("express") && <SiExpress size={20} />}
+        {project.tech.includes("mongo") && <SiMongodb size={20} />}
+        {project.tech.includes("redis") && <SiRedis size={20} />}
+        {project.tech.includes("docker") && <FaDocker size={20} />}
+        {project.tech.includes("tailwind") && <RiTailwindCssFill size={20} />}
       </div>
       <p className="text-lg text-gray-500 my-5">{project?.about}</p>
     </div>
